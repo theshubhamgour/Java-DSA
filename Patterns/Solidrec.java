458cf501d7f7f8915fb0038b4b393c2acf0cb167
theshubhamgour <iamtheshubhamgour@gmail.com>
2022-04-30 17:17:52 +0530

    commit

---
+++ Patterns/Solidrec.java
@@ -0,0 +1,20 @@
+/*
+
+**** 
+**** 
+**** 
+**** 
+
+*/
+package Patterns;
+
+public class Solidrec {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
